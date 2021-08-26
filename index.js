e479a492154281022342f64241c377b4357c7999
Cong Doan <congdoan6997@gmail.com>
2021-08-26 16:03:20 +0300

    install morgan

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/tin', (req, res) => {
 
    return res.send('Hello World!')
